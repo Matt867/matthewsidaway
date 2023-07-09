f09cd2a71fe026df228c32e8a714fbaefc334fb8
Matthew Sidaway <matthewsidaway@outlook.com>
2023-07-09 22:46:38 +0100

    V1

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,248 @@
-import Image from 'next/image'
+"use client";
+
+import {
+  Heading,
+  Card,
+  CardHeader,
+  CardBody,
+  Text,
+  Avatar,
+  Container,
+  Flex,
+  Divider,
+  Image,
+  Tabs,
+  TabList,
+  Tab,
+  Button,
+  TabPanels,
+  TabPanel,
+  Textarea,
+  FormControl,
+  FormLabel,
+  Input,
+} from "@chakra-ui/react";
+
+import { FaGithub } from "react-icons/fa";
+
+import { useState } from "react";
+
+function CompanyCard({
+  companyName,
+  imageSrc,
+  duration,
+  description,
+  children,
+  rootCard = true,
+}: {
+  companyName: string;
+  imageSrc: string;
+  description?: string;
+  duration?: string;
+  children?: React.ReactNode;
+  rootCard?: boolean;
+}) {
+  return (
+    <Card width={"100%"} variant={rootCard ? "outline" : "filled"}>
+      <CardBody>
+        <Flex gap="15px" alignItems="start" direction={"column"}>
+          <Image
+            src={imageSrc}
+            alt={`${companyName} logo`}
+            borderRadius={"lg"}
+          />
+          <Flex direction="column">
+            <Heading size="md">{companyName}</Heading>
+            <Text>Apprentice Software Engineer</Text>
+            <Text color={"GrayText"}>{duration}</Text>
+            <Text>{description}</Text>
+          </Flex>
+          {rootCard ? (
+            <>
+              <Divider mb={"3"} mt={"2"} />
+              <Flex
+                direction={"column"}
+                wrap={"wrap"}
+                gap={"15px"}
+                width={"100%"}
+              >
+                {children}
+              </Flex>
+            </>
+          ) : null}
+        </Flex>
+      </CardBody>
+    </Card>
+  );
+}
+
+function ChildCompanyCard({
+  companyName,
+  imageSrc,
+  duration,
+  description,
+  role = "Apprentice Software Engineer",
+  children,
+}: {
+  companyName: string;
+  imageSrc: string;
+  description?: string;
+  role?: string;
+  duration?: string;
+  children?: React.ReactNode;
+}) {
+  return (
+    <Card variant={"outline"}>
+      <CardBody>
+        <Flex gap="15px" alignItems={"center"} direction={"row"}>
+          <Image
+            src={imageSrc}
+            alt={`${companyName} logo`}
+            borderRadius={"lg"}
+          />
+          <Flex direction="column">
+            <Heading size="md">{companyName}</Heading>
+            <Text>{role}</Text>
+            <Text color={"GrayText"}>{duration}</Text>
+            <Text>{description}</Text>
+          </Flex>
+        </Flex>
+      </CardBody>
+    </Card>
+  );
+}
+
+function ContactForm() {
+  const [data, setData] = useState({ email: "", message: "" });
+
+  return (
+    <Card variant={"outline"}>
+      <CardBody>
+        <FormControl isRequired>
+          <FormLabel>Email address</FormLabel>
+          <Input onChange={(e) => setData({ email: e.target.value, message: data.message })} type="email" />
+        </FormControl>
+        <Divider mt="2vh" mb="1vh" />
+        <FormControl isRequired>
+          <FormLabel>Message</FormLabel>
+          <Textarea onChange={(e) => setData({ email: data.email, message: e.target.value })} height={"xs"} />
+        </FormControl>
+        <Button mt="2vh" width="100%" colorScheme="blue">
+          Submit
+        </Button>
+      </CardBody>
+    </Card>
+  );
+}
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    <Container maxW="container.xl" mb={"2vh"} mt={"2vh"}>
+      <Flex direction="column" gap="2vh">
+        <Card variant={"outline"}>
+          <CardHeader>
+            <Flex alignItems="center" gap="20px">
+              <Avatar size="lg" src="/headshot1.jpg" />
+              <Flex direction="column">
+                <Heading size="2xl">Matthew Sidaway</Heading>
+                <Text>Software Engineer</Text>
+                <Flex mt={"1vh"} gap={"10px"}>
+                  <a href="https://github.com/Matt867">
+                    <Button
+                      colorScheme="gray"
+                      size={"xs"}
+                      leftIcon={<FaGithub />}
+                    >
+                      GitHub
+                    </Button>
+                  </a>
+                </Flex>
+              </Flex>
+            </Flex>
+          </CardHeader>
+        </Card>
+        <Tabs variant={"solid-rounded"}>
+          <TabList flexWrap={"wrap"}>
+            <Tab>Experience</Tab>
+            <Tab>Projects</Tab>
+            <Tab>Education</Tab>
+            <Tab>Contact</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel padding={0}>
+              <Heading size="lg" mt={"2vh"} mb={"2vh"}>
+                Experience
+              </Heading>
+              <Flex direction={"column"} gap={"15px"}>
+                <CompanyCard
+                  companyName="Meta"
+                  imageSrc="/metalogo.png"
+                  duration="Oct 2022 - Present"
+                  rootCard={true}
+                  description="Rotational engineering training program"
+                >
+                  <ChildCompanyCard
+                    companyName="Workplace"
+                    duration="Jan 2023 - Jul 2023"
+                    imageSrc="/workplace.png"
+                  />
+                  <ChildCompanyCard
+                    companyName="Monetization"
+                    imageSrc="/metalogo.png"
+                    duration="Jul 2023 - Jan 2024"
+                  />
+                  <ChildCompanyCard
+                    role="Internship"
+                    companyName="Meta"
+                    duration="Jan 2024 - April 2024"
+                    description="Org TBC"
+                    imageSrc="/metalogo.png"
+                  />
+                </CompanyCard>
+              </Flex>
+            </TabPanel>
+            <TabPanel padding={0}>
+              <Heading size="lg" mt={"2vh"} mb={"2vh"}>
+                Projects
+              </Heading>
+              <Flex direction={"column"} gap={"15px"}>
+                <Card>
+                  <CardHeader>
+                    <Heading size={"md"}>
+                      Hmmm, nothing listed here yet...
+                    </Heading>
+                  </CardHeader>
+                </Card>
+              </Flex>
+            </TabPanel>
+            <TabPanel padding={0}>
+              <Heading size="lg" mt={"2vh"} mb={"2vh"}>
+                Education
+              </Heading>
+              <Flex direction={"column"} gap={"15px"}>
+                <Card>
+                  <CardHeader>
+                    <Heading size={"md"}>
+                      Hmmm, nothing listed here yet...
+                    </Heading>
+                  </CardHeader>
+                </Card>
+              </Flex>
+            </TabPanel>
+            <TabPanel padding={0}>
+              <Heading size="lg" mt={"2vh"} mb={"1vh"}>
+                Contact
+              </Heading>
+              <Text mb={"2vh"}>
+                Leave a message here and I'll get back to you
+              </Text>
+              <Flex direction={"column"} gap={"15px"}>
+                <ContactForm/>
+              </Flex>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Flex>
+    </Container>
+  );
 }
